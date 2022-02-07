a863153556d00972cb9eb5c933e5f7c8ee1d596d
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-02-07 15:51:03 -0500

    Various update
    dbt project

--- models/advanced_config/testing_hooks.sql
+++ models/advanced_config/testing_hooks.sql
@@ -1,3 +1,9 @@
 {{ config(materialized='table', post_hook= 'grant select on {{this}} to role hook_test' ) }}
 
+with hook1 as (
+
 SELECT * FROM "SAMPLE_DATA"."TPCH_SF1"."REGION"
+
+)
+
+SELECT * FROM hook1
